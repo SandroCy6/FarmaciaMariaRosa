dbc117c52d11c3b050938c0cd6b1cdb26d4da894
Alphyyyy <alezito890@gmail.com>
2025-08-26 19:24:13 -0500

    Add dark mode feature with toggle button and corresponding styles

---
+++ assets/js/dark-mode.js
@@ -0,0 +1,16 @@
+const toggleBtn = document.getElementById("darkModeToggle");
+const body = document.body;
+
+if (localStorage.getItem("dark-mode") === "true") {
+body.classList.add("dark-mode");
+}
+toggleBtn.addEventListener("click", () => {
+body.classList.toggle("dark-mode");
+
+// Por qué carajos no funcionaaaaaaaaAAAAAAAAAAAAAAAA ah ya entendi por qué ups
+if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("dark-mode", "true");
+} else {
+    localStorage.setItem("dark-mode", "false");
+}
+});
