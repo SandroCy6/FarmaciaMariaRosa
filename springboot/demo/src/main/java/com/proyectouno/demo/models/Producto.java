e33459fa4b44eca359adadc678bc88a5dfa501b6
AbdielDiazPalomino <abdoljdp@gmail.com>
2025-09-19 09:24:57 -0500

    conect-bbdd-postgres-create-tables

---
+++ springboot/demo/src/main/java/com/proyectouno/demo/models/Producto.java
@@ -0,0 +1,45 @@
+package com.proyectouno.demo.models;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "productos")
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String categoria;
+
+    private BigDecimal precio;
+
+    // Getters y setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+}
+
