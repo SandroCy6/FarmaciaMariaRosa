650f84b86ef250c8717c19dba6201fbe3c2b6ad5
SandroCy6 <sandrodiazzt@gmail.com>
2025-11-10 22:26:46 -0500

    Boton Whatsapp arreglao

--- react-frontend/src/components/WhatsAppFloatingButton/WhatsAppFloatingButton.jsx
+++ react-frontend/src/components/WhatsAppFloatingButton/WhatsAppFloatingButton.jsx
@@ -4,19 +4,13 @@ import styles from "./WhatsAppFloatingButton.module.css";
 export default function WhatsAppFloatingButton() {
   return (
     <a
-      href="https://wa.me/519222684873"
+      href="https://wa.me/51922684873"
       target="_blank"
       rel="noopener noreferrer"
       className={styles.whatsappFloat}
       aria-label="Contacto WhatsApp"
     >
-      <img
-        src="/assets/icons/chat.svg"
-        alt="WhatsApp"
-        width={48}
-        height={48}
-        className={styles.myFloat}
-      />
+      <i className="fa fa-whatsapp" aria-hidden="true"></i>
     </a>
   );
 }
